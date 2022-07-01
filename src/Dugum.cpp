2545678b582bb05b484b17a6b974b8aba73216f9
AliSaidSaritemur <80647702+AliSaidSaritemur@users.noreply.github.com>
2022-07-01 12:25:48 +0300

    odev formatindan cikarildi

--- src/Dugum.cpp
+++ src/Dugum.cpp
@@ -1,11 +1,4 @@
-/**
-* @file g201210044
-* @description Veritxt dosyasýndaki komutlarý yerine getirmek.
-* @course ikinci öðretim  B :2B
-* @assignment 1. Ödev
-* @date 9/11//21
-* @author Ali Said Sarýtemur ali.saritemur@ogr.sakarya.edu.tr
-*/
+
 #include "Dugum.h"
 #include <iostream>
 using namespace std;
